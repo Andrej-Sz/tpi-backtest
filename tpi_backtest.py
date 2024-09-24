6cc2364c5afd8cc2b501116437c50d4bcaf69fc3
Andrej S. <szegeny.andrej.2004@gmail.com>
2024-09-24 09:52:38 +0200

    Create tpi_backtest.py

---
+++ tpi_backtest.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("""
+# TPI Backtest Tool
+
+:building_construction: Under Construction! :building_construction:
+         
+""")
+
+expander_bar = st.expander("About")
+expander_bar.markdown("""
+* This dashboard is build for backtesting TPIs the same way as TradingView Strategies
+""")
